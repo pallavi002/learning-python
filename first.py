03ed13373cdd5fe5a657970acecf55d20c2704ae
pallavi002 <pallavilodhi08@gmail.com>
2020-03-10 00:24:30 +0530

    Python basics initiated

---
+++ first.py
@@ -0,0 +1,30 @@
+print('First python code is running', 'hey you up?', end= '') #end = prints second line on first itself
+print('This is line 2')
+
+print('This is my another try', 'to print a string', 'with sep', sep= '-') #sep puts hyphen in place of every separate string
+
+print('My', 'Name', 'is', 'Pallavi', 'Lodhi', sep = '-', end = '*\n') #puts * at the end if the string 
+
+print('My', 'Name', 'is', 'Pallavi', 'Lodhi', sep = '*', end = '*\n') #puts * at the end if the string and separates by *
+
+print(0.0000000002) #this prints 2e-10
+
+print('I Love "Coding"') #outputs as I Love "Coding"
+
+print('I\'m Pallavi ') #Example of an apostrophe
+
+print(2 ** 3) #it prints 2 raise to 3 which is 8
+print(2 ** 3.5) #it prints 2 raise to 3.5
+print(2.2 ** 3) #it prints 2.2 raise to 3
+print(2.2 ** 3.5) #it prints 2.2 raise to 3.5
+
+# a = True
+# if (a) 
+# {
+#   print(a)
+# } 
+# else 
+# {
+#   print('Sorry')
+# }
+
---
+++ second.py
@@ -0,0 +1,15 @@
+a = 100  #assigns a as 100
+print(a)
+
+a = 3.0
+b = 4.0
+c = (a **  2 + b ** 2) ** 0.5
+print('c = ', c)  #output c = 25.0
+
+#input example
+print('Enter your name..')
+ip = input()
+print('Hello', ip, ',' , 'Welcome to the python world')
+
+something = input('Tell me Something ')
+print('These are your words,' , something)
